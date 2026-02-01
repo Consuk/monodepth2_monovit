601351f30fc8e4b58f76007e2758ad6cd0d5be05
Consuk <0228053@up.edu.mx>
2026-02-01 17:33:55 -0600

    fixing memory alloc mpvit tiny

--- networks/depth_decoder.py
+++ networks/depth_decoder.py
@@ -18,7 +18,7 @@ class DepthDecoder(nn.Module):
         self.scales = scales
 
         self.num_ch_enc = num_ch_enc
-        self.num_ch_dec = np.array([8, 16, 32, 64, 128])
+        self.num_ch_dec = np.array([8, 16, 32, 64, 224]) 
 
         # decoder
         self.convs = OrderedDict()
