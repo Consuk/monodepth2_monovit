efef9c1124133a4a37c37c173eeb481c93eb0c68
Consuk <0228053@up.edu.mx>
2026-02-01 17:29:39 -0600

    fixing memory alloc mpvit tiny

--- networks/depth_decoder.py
+++ networks/depth_decoder.py
@@ -29,12 +29,12 @@ class DepthDecoder(nn.Module):
             self.convs[("upconv", i, 0)] = ConvBlock(num_ch_in, num_ch_out)
 
             # upconv_1
-            if i > 2 and self.use_skips:
+            if self.use_skips and i > 2:
                 num_ch_in = self.num_ch_dec[i] + self.num_ch_enc[i - 1]
             else:
                 num_ch_in = self.num_ch_dec[i]
             num_ch_out = self.num_ch_dec[i]
-
+            self.convs[("upconv", i, 1)] = ConvBlock(num_ch_in, num_ch_out)
 
         for s in self.scales:
             self.convs[("dispconv", s)] = Conv3x3(self.num_ch_dec[s], self.num_output_channels)
